8ec0be046524a832393f3a41c72371f4e320dc3f
xincao9 <xincao9@gmail.com>
2025-01-23 17:06:40 +0800

    备份pre-commit脚本

---
+++ hook/pre-commit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ROOT_DIR=$(git rev-parse --show-toplevel)
+
+cd $ROOT_DIR
+
+mvn formatter:format
+
+if [ $? -ne 0 ]; then
+        echo "Maven 构建格式化，提交中止。"
+        exit 1
+fi
+
+mvn clean install
+
+if [ $? -ne 0 ]; then
+        echo "Maven 构建失败，提交中止。"
+        exit 1
+fi
